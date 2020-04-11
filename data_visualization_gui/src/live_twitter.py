7a652646ab2c4f98042d925ba66ca3a93f4cfc6c
B. Bueno <bbueno25@hotmail.com>
2020-04-10 20:25:41 -0600

    Dynamically Graphing Terms for Sentiment - Data Visualization GUIs with Dash and Python p.10

--- data_visualization_gui/src/live_twitter.py
+++ data_visualization_gui/src/live_twitter.py
@@ -1,8 +1,9 @@
 """
-Live Twitter Sentiment Graph - Data Visualization GUIs with Dash and Python p.9
+Live Twitter Sentiment Graph - Data Visualization GUIs with Dash and Python
 """
 # standard
 import collections
+import input
 import random
 # non-standard
 import dash
@@ -15,10 +16,12 @@ import sqlite3
 app = dash.Dash(__name__)
 
 app.layout = html.Div([html.H2('Live Twitter Sentiment'),
+                       dcc.Input(id='sentiment_term', value='olympic', type='text'),
                        dcc.Graph(id='live-graph', animate=True),
                        dcc.Interval(id='graph-update', interval=1000)])
 
 @app.callback(dash.dependencies.Output('live-graph', 'figure'),
+              [dash.dependencies.Input(component_id='sentiment_term', component_property='value')],
               state=[dash.dependencies.State('graph-update', 'interval')])
 def update_graph_scatter():
     """
@@ -28,17 +31,21 @@ def update_graph_scatter():
         conn = sqlite3.connect('twitter.db')
         c = conn.cursor()
         df = pandas.read_sql(
-            "SELECT * FROM sentiment WHERE tweet LIKE '%olympic%' ORDER BY unix DESC LIMIT 1000",
+            "SELECT * FROM sentiment WHERE tweet LIKE ? ORDER BY unix DESC LIMIT 1000",
             conn)
         df.sort_values('unix', inplace=True)
         df['sentiment_smoothed'] = df['sentiment'].rolling(int(len(df)/5)).mean()
+        df['date'] = pd.to_datetime(df['unix'], unit='ms')
+        df.set_index('date', inplace=True)
+        df = df.resample('1min').mean()
         df.dropna(inplace=True)
-        X = df.unix.values[-100:]
+        X = df.index
         Y = df.sentiment_smoothed.values[-100:]
         data = graph_objs.Scatter(x=X, y=Y, name='Scatter', mode= 'lines+markers')
         return {'data': [data],
                 'layout': graph_objs.Layout(xaxis=dict(range=[min(X), max(X)]),
-                                            yaxis=dict(range=[min(Y), max(Y)]))}
+                                            yaxis=dict(range=[min(Y), max(Y)]),
+                                            title='Term: {}'.format(sentiment_term))}
     except Exception as e:
         with open('errors.txt','a') as f:
             f.write(str(e))
